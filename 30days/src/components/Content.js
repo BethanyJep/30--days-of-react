a51cd8df8fddfd77690316bbf5dfe52354f4b172
[InnCreator] <[engbethany4@gmail.com]>
2020-04-05 00:28:47 +0300

    day 6: create react app

---
+++ 30days/src/components/Content.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+// class Content extends React.Component {
+
+// }
+
+export class Content extends React.Component {
+    // ...
+    render() {
+        return <p className="App-intro">Content goes here</p>;
+    }
+}   
+
+export default Content;
---
+++ 30days/src/components/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+// class Header extends React.Component {
+
+// }
+
+export class Header extends React.Component {
+    // ...
+    render() {
+        return (
+            <div id="header">
+                <h1>Header</h1>
+            </div>
+        );
+    }
+}
+
+export default Header;
