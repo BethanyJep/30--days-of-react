43f3f4a838fe56eb44eac4c52e4046413040db37
[InnCreator] <[engbethany4@gmail.com]>
2020-04-06 00:26:45 +0300

    day 7

--- 30days/src/App.js
+++ 30days/src/App.js
@@ -1,18 +1,15 @@
 import React from 'react';
-import Header from "./components/Header";
-import Content from "./components/Content";
+// import Header from "./components/Header";
+// import Content from "./components/Content";
 
-
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <Header />
-        <Content />
-      </div>
-    );
-  }
-}
-
-
+const a = [1, 10, 100, 1000, 10000];
+const App = props => {
+  return (
+    <ul>
+      {a.map(i => {
+        return <li key={i}>{i}</li>;
+      })}
+    </ul>
+  );
+};
 export default App;
