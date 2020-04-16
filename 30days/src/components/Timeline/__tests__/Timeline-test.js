9c07f6416944e5fe9604b09f84edee0d70b696b7
[InnCreator] <[engbethany4@gmail.com]>
2020-04-17 01:22:03 +0300

    day 17: Testing done

--- 30days/src/components/Timeline/__tests__/Timeline-test.js
+++ 30days/src/components/Timeline/__tests__/Timeline-test.js
@@ -1,8 +1,35 @@
+import React from "react";
+import { shallow, mount } from "enzyme";
+import Timeline from "../Timeline";
 describe("Timeline", () => {
-  it("passing test", () => {
-    expect(true).toBeTruthy();
+  let wrapper;
+  it("wraps content in a div with .notificationsFrame class", () => {
+    wrapper = shallow(<Timeline />);
+    expect(wrapper.find(".notificationsFrame").length).toEqual(1);
   });
-  it("failing test", () => {
-    expect(false).toBeFalsy();
+  it("has a title of Timeline", () => {
+    wrapper = mount(<Timeline />);
+    expect(wrapper.find(".title").text()).toBe("Timeline");
+  });
+  describe("search button", () => {
+    beforeEach(() => (wrapper = mount(<Timeline />)));
+    it("starts out hidden", () => {
+
+      expect(wrapper.find("input.searchInput").hasClass("active")).toBeFalsy
+        ();
+    });
+    it("becomes visible after being clicked on", () => {
+      const icon = wrapper.find(".searchIcon");
+      icon.simulate("click");
+
+      expect(wrapper.find("input.searchInput").hasClass("active")).toBeTruth
+      y();
+    });
+  });
+  describe("status updates", () => {
+    it("has 4 status updates at minimum", () => {
+      wrapper = shallow(<Timeline />);
+      expect(wrapper.find("ActivityItem").length).toBeGreaterThan(3);
+    });
   });
 });
